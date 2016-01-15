0e95631d4a67c38785bffab17f8bd6c637a47b63
Ed King <etking@lin-ek-ml1.stanford.edu>
2016-01-15 12:52:21 -0800

    EM works with sklearn

--- sklearn_native.py
+++ sklearn_native.py
@@ -1,26 +1,39 @@
 import random
+import numpy as np
+
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
-from scipy.sparse import rand
 from nltk.corpus import brown
-from numpy import log, exp, isnan, isinf, ceil, sum
+from numpy import log, exp, isnan, isinf, ceil, sum, resize
 from sklearn.utils.extmath import logsumexp
 
+NRUNS = 50
+
+def count_docs_per_class(nb):
+    return nb.predict_proba(doc_vec).sum(axis=0)
+
+def count_live_classes(nb):
+    docs_per_class = count_docs_per_class(nb)
+    return len(docs_per_class.nonzero()[0])
+
 class NaiveBayesEM(object):
-    def __init__(self, documents, n_categories, randomize=False, max_iterations=20):
+    def __init__(self, documents, n_categories, max_iterations=20, randomize=False):
         self.documents = documents
-        self.vocab = self.generate_vocab()
-
-        self.categories = range(n_categories)
-
-        self.p_categories = []
-        self.p_words_by_category = []
+        self.n_categories = n_categories
+        
+        self.n_samples, self.n_features = self.documents.shape
+        
+        self.class_log_prior_shape = (self.n_categories,)
+        self.feature_log_prob_shape = (self.n_categories, self.n_features)
+        
+        self.class_log_priors = []
+        self.feature_log_probs = []
 
         self.randomize = randomize
 
+        # when/how to stop the EM iterations
         self.max_iterations = max_iterations
         self.likelihood = []
-        self.all_nb = []
 
     def runEM(self):
         self.initializeEM()
@@ -32,90 +45,74 @@ class NaiveBayesEM(object):
             except IndexError: prev_likelihood = None
 
             nb = MultinomialNB(self.documents, None)
-            nb.categories = self.categories
-            self.all_nb.append(nb)
+            # add faked "classes_" attribute to force it to think it's been trained
+            nb.classes_ = np.ndarray((self.n_categories,))
+            # and add the random parameters to actually "train" it
+            nb.class_log_prior_ = self.class_log_priors[-1]
+            nb.feature_log_prob_ = self.feature_log_probs[-1]
 
-            self.e_step(nb)
-            self.m_step(nb)
-            #del nb
-
+            soft_predictions = self.e_step(nb)
+            nb = self.m_step(soft_predictions)
+            
+            self.last_nb = nb
+            
             ### CHECK LIKELIHOOD CHANGE
-            this_likelihood = nb.calculate_likelihood()
+            jll = nb._joint_log_likelihood(self.documents)
+            best_likelihoods = jll.max(axis=1)
+            this_likelihood = sum(best_likelihoods)
+            
             self.likelihood.append(this_likelihood)
             if this_likelihood == prev_likelihood:                
                 done = True
+                pass
 
-            print iter_n, this_likelihood, nb.count_classifications()
+            #print iter_n, this_likelihood #, nb.count_classifications()
+            #print iter_n, this_likelihood, count_live_classes(nb)
             if done:
                 break
 
     def initializeEM(self):
-        p_categories = {}
-        for c in self.categories:
-            randfloat = random.random()
-            if self.randomize:
-                p_categories[c] = randfloat
-            else:
-                # make it mostly equal
-                p_categories[c] = float(1+(0.05*randfloat)) / len(self.categories)
-        p_categories = scale_dictionary(p_categories)
-
-        p_words_by_category = {}
-        for c in self.categories:
-            cwords = {}
-            for w in self.vocab:
-                randfloat = random.random()
-                if self.randomize:
-                    cwords[w] = random.random()
-                else:
-                    wordcount = self.vocab[w]
-                    cwords[w] = float(wordcount+(0.05*randfloat)) / len(self.vocab)
-            p_words_by_category[c] = scale_dictionary(cwords)
-
-        self.p_categories.append(p_categories)
-        self.p_words_by_category.append(p_words_by_category)
+        if self.randomize:
+            class_log_prior_ = generate_normed_rand_log_prob(self.class_log_prior_shape)
+            feature_log_prob_ = generate_normed_rand_log_prob(self.feature_log_prob_shape)
+        else:
+            uniform_class_counts = np.ones(self.class_log_prior_shape)
+            class_log_prior_ = generate_normed_rand_log_prob(self.class_log_prior_shape,
+                                                             count_vec=uniform_class_counts,
+                                                             max_alpha=0)
+            
+            doc_vec_counts = resize(self.documents.sum(0), self.feature_log_prob_shape)
+            feature_log_prob_ = generate_normed_rand_log_prob(self.feature_log_prob_shape, 
+                                                              count_vec=doc_vec_counts, 
+                                                              max_alpha=10)
+        
+        self.class_log_priors.append(class_log_prior_)
+        self.feature_log_probs.append(feature_log_prob_)
 
     def e_step(self, nb):
-        nb.p_categories = self.p_categories[-1]
-        nb.p_words_by_category = self.p_words_by_category[-1]
+        nb.class_log_prior_ = self.class_log_priors[-1]
+        nb.feature_log_prob_ = self.feature_log_probs[-1]
 
-        nb.doc_categories = []
+        soft_predictions = nb.predict_proba(self.documents)
+        return soft_predictions
 
-        for doc in self.documents:
-            doc_class = nb.soft_classify(doc)
-            #nb.likelihood += sum(doc_class.values())
-            nb.doc_categories.append(doc_class)
-
-    def m_step(self, nb):
-        nb.train()
-        self.p_categories.append(nb.p_categories)
-        self.p_words_by_category.append(nb.p_words_by_category)
-
-    def generate_vocab(self):
-        vocab = {}
-
-        for doc in self.documents:
-            for word in doc:
-                vocab[word] = doc[word]
-                
-        return vocab
-    
-    def calculate_category_words(self, n=10):
-        nb = self.all_nb[-1]
-        predictions = nb.count_classifications()
+    def m_step(self, soft_predictions):
+        nb = MultinomialNB()
         
-        baseline = self.vocab
-        
-        good_cats = {}
-        
-        for c in predictions:
-            c_worddict = nb.p_words_by_category[c]
-            diff_from_baseline = [(w, c_worddict[w] - baseline[w]) for w in baseline]
-            diff_from_baseline.sort(cmp=lambda x,y: cmp(x[1],y[1]))
+        for category_idx in range(self.n_categories):
+            catvec = np.zeros(self.n_samples)
+            catvec += category_idx
             
-            good_cats[c] = diff_from_baseline[:n] + diff_from_baseline[-n:]
+            cat_weights = soft_predictions.T[category_idx]
+            nb.partial_fit(self.documents, 
+                           catvec, 
+                           classes=range(self.n_categories),
+                           sample_weight=cat_weights)
+            
+        self.class_log_priors.append(nb.class_log_prior_)
+        self.feature_log_probs.append(nb.feature_log_prob_)
         
-        return good_cats
+        return nb
             
 def build_all_brown(subset=False):
     documents = []
@@ -141,13 +138,37 @@ def build_all_brown(subset=False):
     else:
         return documents, categories
 
+def generate_normed_rand_log_prob(vecshape, count_vec=None, max_alpha=10):
+    if count_vec is None:
+        rand_prob = np.random.random(vecshape)
+    else:
+        rand_to_add = ceil(np.random.random(vecshape) * max_alpha)
+        rand_prob = count_vec + rand_to_add
+    rand_log_prob = log(rand_prob)
+    
+    norm_axis = vecshape.index(max(vecshape))
+    log_norm = logsumexp(rand_log_prob, norm_axis)
+    log_norm_vec = resize(log_norm, vecshape)
+    
+    normed_rand_log_prob = rand_log_prob - log_norm_vec
+    
+    return normed_rand_log_prob
+
 if __name__ == '__main__':
-    skdocs, skcats = build_all_brown(True)
+    skdocs, skcats = build_all_brown(False)
     skdocs = [' '.join(d) for d in skdocs]
     
     doc_vectorizer = CountVectorizer(skdocs)
     doc_vectorizer.fit(skdocs)
-    
-    sknb = MultinomialNB()
     doc_vec = doc_vectorizer.transform(skdocs)
-    sknb.fit(doc_vec, skcats)
+
+    # simulate the likelihood landscape
+    for run_number in range(NRUNS):
+        nbem = NaiveBayesEM(doc_vec, 15)
+        nbem.runEM()
+        
+        nb = nbem.last_nb
+        live_classes = count_live_classes(nb)
+        likelihood = nbem.likelihood[-1]
+        
+        print "run %s: %s,%s" % (run_number, live_classes, likelihood)
